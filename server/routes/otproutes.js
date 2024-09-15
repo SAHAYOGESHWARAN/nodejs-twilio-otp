8b06d453e05bce4b45a57204a3371cfe411a7ab9
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-09-15 21:05:23 +0530

    otp

--- server/routes/otproutes.js
+++ server/routes/otproutes.js
@@ -1,10 +1,9 @@
-const express = require("express");
-const router = express.Router(); 
-const { sendOtp, verifyOtp } = require("../controller/otpcontroller"); // Import OTP controller
+const express = require('express');
+const { sendOtp, verifyOtp } = require('../controllers/otpController');
 
-// Define routes for sending and verifying OTP
-router.post("/send-otp", sendOtp);
-router.post("/verify-otp", verifyOtp);
+const router = express.Router();
 
-// Export the router so it can be used in other parts of the application
+router.post('/send-otp', sendOtp);
+router.post('/verify-otp', verifyOtp);
+
 module.exports = router;
